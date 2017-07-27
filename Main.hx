96b8cc2fc88fb09f8a1b22680a3c601003154ff1
migmit <migmit@gmail.com>
2017-07-27 23:34:58 +0200

    Added Maybe

--- Main.hx
+++ Main.hx
@@ -1,5 +1,34 @@
+using Main.MaybeExt;
+
+enum Maybe<T> {
+  Just(value: T);
+  Nothing;
+}
+
+class MaybeExt {
+  static public function getOrElse<T>(m: Maybe<T>, other: T): T {
+    switch(m) {
+      case Just(v): return v;
+      case Nothing: return other;
+    }
+  }
+  static public function map<T,U>(m: Maybe<T>, f: T -> U): Maybe<U> {
+    switch(m) {
+      case Just(v): return Just(f(v));
+      case Nothing: return Nothing;
+    }
+  }
+  static public function flatMap<T,U>(m: Maybe<T>, f: T -> Maybe<U>): Maybe<U> {
+    return m.map(f).getOrElse(Nothing);
+  }
+}
+
 class Main {
   static public function main() {
-    trace("Test");
+    var a = Just("abc");
+    var b = Nothing;
+    trace(a.getOrElse("Test"));
+    trace(b.getOrElse("Test"));
+    trace(a.map(function(s) return s.length).getOrElse(-1));
   }
 }
