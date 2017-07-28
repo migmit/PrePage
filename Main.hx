498827c86bec9dcd1813921ee53677570baf0270
migmit <migmit@gmail.com>
2017-07-28 10:50:26 +0200

    ArrayExt, StringExt etc

--- Main.hx
+++ Main.hx
@@ -1,4 +1,6 @@
 using Main.MaybeExt;
+using Main.ArrayExt;
+using Main.StringExt;
 
 enum Maybe<T> {
   Just(value: T);
@@ -20,6 +22,57 @@ class MaybeExt {
   }
   static public function flatMap<T,U>(m: Maybe<T>, f: T -> Maybe<U>): Maybe<U> {
     return m.map(f).getOrElse(Nothing);
+  }
+}
+
+class Pair<T,U> {
+  public var fst(default, null): T;
+  public var snd(default, null): U;
+  public function new(fst: T, snd: U) {
+    this.fst = fst;
+    this.snd = snd;
+  }
+}
+
+class ArrayExt {
+  static public function find<T>(a: Array<T>, p: T -> Bool): Maybe<T> {
+    for (i in 0...a.length) {
+      var t = a[i];
+      if (p(t)) return Just(t);
+    }
+    return Nothing;
+  }
+  static public function findLast<T>(a: Array<T>, p: T -> Bool): Maybe<T> {
+    var l = a.length;
+    for (i in 0...l) {
+      var t = a[l-i-1];
+      if (p(t)) return Just(t);
+    }
+    return Nothing;
+  }
+  static public function foldLeft<T, U>(a: Array<T>, z: U, op: U -> T -> U): U {
+    var r = z;
+    for (i in 0...a.length) {
+      r = op(r, a[i]);
+    }
+    return r;
+  }
+}
+
+class StringExt {
+  public static function trimRight(s: String) {
+    var l = s.length;
+    for (i in 0...l) {
+      var p = l-i-1;
+      if (s.charAt(p) != " ") return s.substring(0,p+1);
+    }
+    return "";
+  }
+  public static function trimLeft(s: String) {
+    for (i in 0...s.length) {
+      if (s.charAt(i) != " ") return s.substring(i);
+    }
+    return "";
   }
 }
 
@@ -59,6 +112,17 @@ class Slice<S> {
     this.style = style;
     this.action = action;
   }
+  public function length(): Int {
+    return text.length;
+  }
+  public function split(index: Int): Pair<Slice<S>, Slice<S>> {
+    return new Pair(new Slice(text.substring(0, index).trimRight(), style, action), new Slice(text.substring(index).trimLeft(), style, action));
+  }
+  public function spaceSplit(lastIndex: Int): Maybe<Pair<Slice<S>, Slice<S>>> {
+    var pos = text.lastIndexOf(" ", lastIndex);
+    if (pos < 0) return Nothing;
+    return Just(new Pair(new Slice(text.substring(0, pos).trimRight(), style, action), new Slice(text.substring(pos+1).trimLeft(), style, action)));
+  }
 }
 
 class Text<S> {
@@ -66,17 +130,27 @@ class Text<S> {
   public function new(value: Array<Slice<S>>) {
     this.value = value;
   }
+  public function length(): Int {
+    return value.foldLeft(0, function(n, s) return n + s.length());
+  }
 }
 
 class Main {
   static public function main() {
     var a = Just("abc");
     var b = Nothing;
-    var s = new Slice<String>("xyz", new TextStyle(false, false, new TextColor(0,0,0)), Nothing);
-    var t = new Text([s]);
+    var s = new Slice<String>("xyz   tuv f", new TextStyle(false, false, new TextColor(0,0,0)), Nothing);
+    var t = new Text([s,s]);
     trace(a.getOrElse("Test"));
     trace(b.getOrElse("Test"));
     trace(a.map(function(s) return s.length).getOrElse(-1));
     trace(t);
+    trace([0,1,2,3,4].find(function(n) return n % 3 == 0).getOrElse(-1));
+    trace([0,1,2,3,4].findLast(function(n) return n % 3 == 0).getOrElse(-1));
+    trace(s.spaceSplit(2));
+    trace(s.spaceSplit(4));
+    trace(s.spaceSplit(9));
+    trace(s.split(8));
+    trace(t.length());
   }
 }
