f30335417000145ea718b3cad0276f0af30357ba
Mikhail Mitrofanov <mikhail.mitrofanov@gizmodomedia.com>
2017-07-28 14:24:42 +0200

    Some more simplifications

--- Main.hx
+++ Main.hx
@@ -1,11 +1,10 @@
 import text.Slice;
 import text.Text;
-import text.TextColor;
 import text.TextStyle;
 
 class Main {
   static public function main() {
-    var s = new Slice<String>("xyz   tuv f", new TextStyle(false, false, new TextColor(0,0,0)), Nothing);
+    var s = new Slice<String>("xyz   tuv f", TextStyle.dflt, Nothing);
     var t = new Text([s,s]);
     for (i in 1...23) {
       trace(i);
